5c4e04327c2d52320e532823a05d625f3e3e3801
Alexandre Silva <xande1231231@hotmail.com>
2019-07-10 15:03:27 -0300

    Initial project

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName = "Card")]
+    public class Card : ScriptableObject {
+        public CardType cardType;
+        public CardProperties[] properties;
+    }
+}
